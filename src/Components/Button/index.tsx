1b87c52372bd4dfe059759279b3eedcf0b778ad4
Alex Edwards <alexedwards.ac@gmail.com>
2022-12-18 09:20:28 -0600

    added props to button

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const Button = ({ children }: IButtonProps) => {
-  return <button>{children}</button>;
+export const Button = ({ children, ...props }: IButtonProps) => {
+  return <button {...props}>{children}</button>;
 };
